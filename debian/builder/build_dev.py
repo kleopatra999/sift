bec31601004ebed36090fa1814eab901d50e6573
Erik <erik@erikkristensen.com>
2013-12-29 10:08:15 -0500

    prepping for another build

--- debian/builder/build_dev.py
+++ debian/builder/build_dev.py
@@ -5,7 +5,7 @@ packages = []
 with open('packages.txt', 'r') as f:
   for line in f:
     pkg = line.split(',')
-    packages.append(pkg[0].strip() + " (>= " + pkg[1].strip() + ")")
+    packages.append(pkg[0].strip())
 
 
 print ', '.join(packages)
